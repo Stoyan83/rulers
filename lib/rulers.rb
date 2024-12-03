79b52dee6c5f807270a0318350e72d85094b025a
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-12-03 14:03:21 +0200

    simple Rack settings in ruler gem

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,6 +3,13 @@
 require_relative "rulers/version"
 
 module Rulers
+  class Application
+    def call(env)
+      [200, {'content-type' => 'text/html'},
+        ["Hello from Ruby on Rulers!"]]
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
