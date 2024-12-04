cf481c2d0da46f4112fa4383ad2d3364119bcc16
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-12-04 13:45:46 +0200

    Learning ActiveSupoort functinality

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require_relative 'rulers/array'
 
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'content-type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
     end
