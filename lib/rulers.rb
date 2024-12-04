1117f668e5b5b19aef718777e1a3fe7e67386376
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-12-04 14:08:35 +0200

    Fixing error

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "rulers/version"
 require_relative 'rulers/array'
+require_relative "rulers/version"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
       [200, {'content-type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
     end
